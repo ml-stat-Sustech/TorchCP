2d4bfae5d72f984b7d8e33964b763a500a3b0f30
huangjg <jontyhuang@gmail.com>
2023-12-09 10:46:59 +0800

    reorganize files

--- deepcp_examples/imagenet_thr_standard.py
+++ deepcp_examples/imagenet_thr_standard.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2023-present, (Nan).
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
 import os
 from tqdm import tqdm
 import pickle 
