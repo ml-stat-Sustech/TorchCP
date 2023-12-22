6a45d52f4beb80137edc43d0cee212178f55fe77
huangjg <jontyhuang@gmail.com>
2023-12-22 20:49:14 +0800

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,37 @@ import sys
 import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../../'))
 
+from unittest.mock import Mock  # noqa: F401, E402
+# from sphinx.ext.autodoc.importer import _MockObject as Mock
+Mock.Module = object
+sys.modules['torch'] = Mock()
+sys.modules['numpy'] = Mock()
+sys.modules['numpy.linalg'] = Mock()
+sys.modules['scipy'] = Mock()
+sys.modules['scipy.optimize'] = Mock()
+sys.modules['scipy.interpolate'] = Mock()
+sys.modules['scipy.ndimage'] = Mock()
+sys.modules['scipy.ndimage.filters'] = Mock()
+sys.modules['tensorflow'] = Mock()
+sys.modules['theano'] = Mock()
+sys.modules['theano.tensor'] = Mock()
+sys.modules['torch'] = Mock()
+sys.modules['torch.autograd'] = Mock()
+sys.modules['torch.autograd.gradcheck'] = Mock()
+sys.modules['torch.distributions'] = Mock()
+sys.modules['torch.nn'] = Mock()
+sys.modules['torch.nn.functional'] = Mock()
+sys.modules['torch.optim'] = Mock()
+sys.modules['torch.nn.modules'] = Mock()
+sys.modules['torch.nn.modules.utils'] = Mock()
+sys.modules['torch.nn.modules.loss'] = Mock()
+sys.modules['torch.utils'] = Mock()
+sys.modules['torch.utils.model_zoo'] = Mock()
+sys.modules['torch.nn.init'] = Mock()
+sys.modules['torch.utils.data'] = Mock()
+sys.modules['torchvision'] = Mock()
+sys.modules['randomstate'] = Mock()
+sys.modules['scipy._lib'] = Mock()
 import torchcp
 
 project = 'TorchCP'
