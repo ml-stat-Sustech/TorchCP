df29098836ffcc5e61b04672e23ac9bd1c97d6a8
xuanningzhou <shinning_zhou@163.com>
2024-09-04 14:35:02 +0000

    add CQRR and CQRM algorithm

--- torchcp/regression/predictors/cqr.py
+++ torchcp/regression/predictors/cqr.py
@@ -40,3 +40,71 @@ class CQR(SplitPredictor):
         prediction_intervals[..., 0] = predicts_batch[..., 0] - self.q_hat.view(1, self.q_hat.shape[0], 1)
         prediction_intervals[..., 1] = predicts_batch[..., 1] + self.q_hat.view(1, self.q_hat.shape[0], 1)
         return prediction_intervals
+
+
+class CQRR(CQR):
+    """
+    A comparison of some conformal quantile regression methods (Matteo Sesia and Emmanuel J. Candes, 2019)
+    paper: https://arxiv.org/abs/1909.05433
+
+    :param model: a pytorch model that can output alpha/2 and 1-alpha/2 quantile regression.
+    """
+    
+    def calculate_score(self, predicts, y_truth):
+        if len(predicts.shape) == 2:
+            predicts = predicts.unsqueeze(1)
+        if len(y_truth.shape) == 1:
+            y_truth = y_truth.unsqueeze(1)
+        eps = 1e-6
+        scaling_factor = predicts[..., 1] - predicts[..., 0] + eps
+        return torch.maximum((predicts[..., 0] - y_truth) / scaling_factor, (y_truth - predicts[..., 1]) / scaling_factor)
+    
+    def predict(self, x_batch):
+        self._model.eval()
+        if len(x_batch.shape) == 1:
+            x_batch = x_batch.unsqueeze(0)
+        predicts_batch = self._model(x_batch.to(self._device)).float()
+        if len(predicts_batch.shape) == 2:
+            predicts_batch = predicts_batch.unsqueeze(1)
+        prediction_intervals = x_batch.new_zeros((predicts_batch.shape[0], self.q_hat.shape[0], 2))
+        
+        eps = 1e-6
+        scaling_factor = prediction_intervals[..., 1] - prediction_intervals[..., 0] + eps
+        prediction_intervals[..., 0] = predicts_batch[..., 0] - self.q_hat.view(1, self.q_hat.shape[0], 1) * scaling_factor
+        prediction_intervals[..., 1] = predicts_batch[..., 1] + self.q_hat.view(1, self.q_hat.shape[0], 1) * scaling_factor
+        return prediction_intervals
+
+
+class CQRM(CQR):
+    """
+    Adaptive, Distribution-Free Prediction Intervals for Deep Networks (Kivaranovic et al., 2019)
+    paper: https://arxiv.org/abs/1905.10634
+
+    :param model: a pytorch model that can output alpha/2, 1/2 and 1-alpha/2 quantile regression.
+    """
+    
+    def calculate_score(self, predicts, y_truth):
+        if len(predicts.shape) == 2:
+            predicts = predicts.unsqueeze(1)
+        if len(y_truth.shape) == 1:
+            y_truth = y_truth.unsqueeze(1)
+        eps = 1e-6
+        scaling_factor_lower = predicts[..., 1] - predicts[..., 0] + eps
+        scaling_factor_upper = predicts[..., 2] - predicts[..., 1] + eps
+        return torch.maximum((predicts[..., 0] - y_truth) / scaling_factor_lower, (y_truth - predicts[..., 2]) / scaling_factor_upper)
+    
+    def predict(self, x_batch):
+        self._model.eval()
+        if len(x_batch.shape) == 1:
+            x_batch = x_batch.unsqueeze(0)
+        predicts_batch = self._model(x_batch.to(self._device)).float()
+        if len(predicts_batch.shape) == 2:
+            predicts_batch = predicts_batch.unsqueeze(1)
+        prediction_intervals = x_batch.new_zeros((predicts_batch.shape[0], self.q_hat.shape[0], 2))
+        
+        eps = 1e-6
+        scaling_factor_lower = predicts_batch[..., 1] - predicts_batch[..., 0] + eps
+        scaling_factor_upper = predicts_batch[..., 2] - predicts_batch[..., 1] + eps
+        prediction_intervals[..., 0] = predicts_batch[..., 0] - self.q_hat.view(1, self.q_hat.shape[0], 1) * scaling_factor_lower
+        prediction_intervals[..., 1] = predicts_batch[..., 2] + self.q_hat.view(1, self.q_hat.shape[0], 1) * scaling_factor_upper
+        return prediction_intervals
