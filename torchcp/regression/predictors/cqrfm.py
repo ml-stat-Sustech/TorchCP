ad80faf8a90eadb34b7b4a4ec86ec8da9e16d0df
xuanningzhou <shinning_zhou@163.com>
2024-09-09 14:14:35 +0000

    refactor(regression): update cqr and its variants style

---
+++ torchcp/regression/predictors/cqrfm.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2023-present, SUSTech-ML.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import torch
+
+from .cqrm import CQRM
+
+
+class CQRFM(CQRM):
+    """
+    Adaptive, Distribution-Free Prediction Intervals for Deep Networks (Kivaranovic et al., 2019)
+    paper: https://proceedings.mlr.press/v108/kivaranovic20a.html
+
+    :param model: a pytorch model that can output alpha/2, 1/2 and 1-alpha/2 quantile regression.
+    """
+
+    def calculate_score(self, predicts, y_truth):
+        if len(predicts.shape) == 2:
+            predicts = predicts.unsqueeze(1)
+        if len(y_truth.shape) == 1:
+            y_truth = y_truth.unsqueeze(1)
+        return torch.maximum((predicts[..., 1] - y_truth) / (predicts[..., 1] - predicts[..., 0]),
+                             (y_truth - predicts[..., 1]) / (predicts[..., 2] - predicts[..., 1]))
+
+    def predict(self, x_batch):
+        self._model.eval()
+        if len(x_batch.shape) == 1:
+            x_batch = x_batch.unsqueeze(0)
+        predicts_batch = self._model(x_batch.to(self._device)).float()
+        if len(predicts_batch.shape) == 2:
+            predicts_batch = predicts_batch.unsqueeze(1)
+        prediction_intervals = x_batch.new_zeros((predicts_batch.shape[0], self.q_hat.shape[0], 2))
+
+        prediction_intervals[..., 0] = predicts_batch[..., 1] - self.q_hat.view(1, self.q_hat.shape[0], 1) * (
+                    predicts_batch[..., 1] - predicts_batch[..., 0])
+        prediction_intervals[..., 1] = predicts_batch[..., 1] + self.q_hat.view(1, self.q_hat.shape[0], 1) * (
+                    predicts_batch[..., 2] - predicts_batch[..., 1])
+        return prediction_intervals
