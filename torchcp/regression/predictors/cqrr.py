fba7c4289cb3f501741ec38c9c6af266e433f42e
xuanningzhou <shinning_zhou@163.com>
2024-09-10 13:44:48 +0000

    refactor(regression): refactor regression algorithm to adapt ensemble score_predictor

--- torchcp/regression/predictors/cqrr.py
+++ torchcp/regression/predictors/cqrr.py
@@ -28,19 +28,13 @@ class CQRR(CQR):
         return torch.maximum((predicts[..., 0] - y_truth) / scaling_factor,
                              (y_truth - predicts[..., 1]) / scaling_factor)
 
-    def predict(self, x_batch):
-        self._model.eval()
-        if len(x_batch.shape) == 1:
-            x_batch = x_batch.unsqueeze(0)
-        predicts_batch = self._model(x_batch.to(self._device)).float()
+    def generate_intervals(self, predicts_batch, q_hat):
         if len(predicts_batch.shape) == 2:
             predicts_batch = predicts_batch.unsqueeze(1)
-        prediction_intervals = x_batch.new_zeros((predicts_batch.shape[0], self.q_hat.shape[0], 2))
+        prediction_intervals = predicts_batch.new_zeros((predicts_batch.shape[0], q_hat.shape[0], 2))
 
         eps = 1e-6
         scaling_factor = prediction_intervals[..., 1] - prediction_intervals[..., 0] + eps
-        prediction_intervals[..., 0] = predicts_batch[..., 0] - self.q_hat.view(1, self.q_hat.shape[0],
-                                                                                1) * scaling_factor
-        prediction_intervals[..., 1] = predicts_batch[..., 1] + self.q_hat.view(1, self.q_hat.shape[0],
-                                                                                1) * scaling_factor
+        prediction_intervals[..., 0] = predicts_batch[..., 0] - q_hat.view(1, q_hat.shape[0], 1) * scaling_factor
+        prediction_intervals[..., 1] = predicts_batch[..., 1] + q_hat.view(1, q_hat.shape[0], 1) * scaling_factor
         return prediction_intervals
