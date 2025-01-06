d2153aeead9ef64c4ef4491a24d95ec1748ac7e0
janqsong <janqsong@gmail.com>
2025-01-06 16:38:01 +0800

    fix(ConfLearn)

--- examples/conformal_learning.py
+++ examples/conformal_learning.py
@@ -18,6 +18,7 @@ from examples.utils import Model_Ex1, get_others_dir
 from torchcp.classification.predictor import SplitPredictor
 from torchcp.classification.score import APS
 
+
 class ClassNNet(nn.Module):
     def __init__(self, num_features, num_classes, use_dropout=False):
         super(ClassNNet, self).__init__()
@@ -59,18 +60,19 @@ class ClassNNet(nn.Module):
             x = self.batchnorm3(x)
         x = self.layer_4(x)
         x = self.relu(x)
-        
+
         if self.use_dropout:
             x = self.dropout(x)
             x = self.batchnorm4(x)
 
         x = self.layer_5(x)
-           
+
         if extract_features:
-            return x, torch.cat([z2,z3],1)
+            return x, torch.cat([z2, z3], 1)
         else:
             return x
-        
+
+
 class ClassifierDataset(Dataset):
     def __init__(self, X_data, y_data, z_data):
         self.X_data = X_data
@@ -80,9 +82,9 @@ class ClassifierDataset(Dataset):
     def __getitem__(self, index):
         return self.X_data[index], self.y_data[index], self.z_data[index]
 
-    def __len__ (self):
+    def __len__(self):
         return len(self.X_data)
-    
+
 
 class CommonDataset(Dataset):
     def __init__(self, X_data, Y_data):
@@ -92,67 +94,70 @@ class CommonDataset(Dataset):
     def __getitem__(self, index):
         return self.X_data[index], self.Y_data[index]
 
-    def __len__ (self):
+    def __len__(self):
         return len(self.X_data)
-    
+
 
 class Oracle:
     def __init__(self, model):
         self.model = model
-    
+
     def fit(self, X, y):
         return self
 
     def predict(self, X):
-        return self.model.sample_Y(X)        
+        return self.model.sample_Y(X)
 
     def predict_proba(self, X):
-        if(len(X.shape)==1):
-            X = X.reshape((1,X.shape[0]))
+        if (len(X.shape) == 1):
+            X = X.reshape((1, X.shape[0]))
         prob = self.model.compute_prob(X)
         prob = np.clip(prob, 1e-6, 1.0)
-        prob = prob / prob.sum(axis=1)[:,None]
+        prob = prob / prob.sum(axis=1)[:, None]
         return prob
-    
+
 
 def difficulty_oracle(S_oracle, size_cutoff=1):
     size_oracle = np.array([len(S) for S in S_oracle])
-    easy_idx = np.where(size_oracle<=size_cutoff)[0]
-    hard_idx = np.where(size_oracle>size_cutoff)[0]
+    easy_idx = np.where(size_oracle <= size_cutoff)[0]
+    hard_idx = np.where(size_oracle > size_cutoff)[0]
     return easy_idx, hard_idx
 
 
 def evaluate_predictions(S, X, y, hard_idx=None, conditional=True, linear=False):
     # Marginal coverage
     marg_coverage = np.mean([y[i] in S[i] for i in range(len(y))])
-        
+
     if conditional:
         y_hard = y[hard_idx]
         S_hard = [S[i] for i in hard_idx]
 
         # Evaluate conditional coverage
-        wsc_coverage = np.mean([y_hard[i] in S_hard[i] for i in range(len(y_hard))])
+        wsc_coverage = np.mean([y_hard[i] in S_hard[i]
+                               for i in range(len(y_hard))])
 
         # Evaluate conditional size
         size_hard = np.mean([len(S[i]) for i in hard_idx])
-        size_easy = np.mean([len(S[i]) for i in range(len(y)) if i not in hard_idx])   
+        size_easy = np.mean([len(S[i])
+                            for i in range(len(y)) if i not in hard_idx])
         size_hard_median = np.median([len(S[i]) for i in hard_idx])
-        size_easy_median = np.median([len(S[i]) for i in range(len(y)) if i not in hard_idx])   
+        size_easy_median = np.median(
+            [len(S[i]) for i in range(len(y)) if i not in hard_idx])
 
         n_hard = len(hard_idx)
         n_easy = len(y) - len(hard_idx)
-            
+
     else:
         wsc_coverage = None
 
     # Size and size conditional on coverage
-    size = np.mean([len(S[i]) for i in range(len(y))])     
+    size = np.mean([len(S[i]) for i in range(len(y))])
     size_median = np.median([len(S[i]) for i in range(len(y))])
     idx_cover = np.where([y[i] in S[i] for i in range(len(y))])[0]
     size_cover = np.mean([len(S[i]) for i in idx_cover])
     # Combine results
     out = pd.DataFrame({'Coverage': [marg_coverage], 'Conditional coverage': [wsc_coverage],
-                        'Size': [size], 'Size (median)': [size_median], 
+                        'Size': [size], 'Size (median)': [size_median],
                         'Size-hard': [size_hard], 'Size-easy': [size_easy],
                         'Size-hard (median)': [size_hard_median], 'Size-easy (median)': [size_easy_median],
                         'n-hard': [n_hard], 'n-easy': [n_easy],
@@ -162,11 +167,12 @@ def evaluate_predictions(S, X, y, hard_idx=None, conditional=True, linear=False)
 
 def eval_predictions(X, Y, box, data="unknown", printing=True):
     Y_pred = box.predict(X)
-    class_error = np.mean(Y!=Y_pred)
+    class_error = np.mean(Y != Y_pred)
     if printing:
-        print("Classification error on {:s} data: {:.1f}%".format(data, class_error*100))
-    return (class_error*100)
-    
+        print("Classification error on {:s} data: {:.1f}%".format(
+            data, class_error * 100))
+    return (class_error * 100)
+
 
 def setup_data_and_model(device):
     p = 100         # Number of features
@@ -177,25 +183,33 @@ def setup_data_and_model(device):
 
     n_train = 4800  # Number of data samples
     n_hout = 2000   # Number of hold out samples
-    n_calib = 10000 # Number of calibration samples
+    n_calib = 10000  # Number of calibration samples
     n_test = 2000   # Number of test samples
 
     data_model = Model_Ex1(K, p, delta_1, delta_2, a)   # Data generating model
 
-    X_train = data_model.sample_X(n_train)              # Generate the data features
-    Y_train = data_model.sample_Y(X_train)              # Generate the data labels conditional on the features
+    # Generate the data features
+    X_train = data_model.sample_X(n_train)
+    # Generate the data labels conditional on the features
+    Y_train = data_model.sample_Y(X_train)
 
-    n_tr_score = int(n_train * 0.2)                     # Number of data samples for training the new loss
-    X_tr_score = data_model.sample_X(n_tr_score)        # Generate the data features
-    Y_tr_score = data_model.sample_Y(X_tr_score)        # Generate the data labels conditional on the features
+    # Number of data samples for training the new loss
+    n_tr_score = int(n_train * 0.2)
+    # Generate the data features
+    X_tr_score = data_model.sample_X(n_tr_score)
+    # Generate the data labels conditional on the features
+    Y_tr_score = data_model.sample_Y(X_tr_score)
 
-    X_hout = data_model.sample_X(n_hout)                # Generate independent hold-out data
+    # Generate independent hold-out data
+    X_hout = data_model.sample_X(n_hout)
     Y_hout = data_model.sample_Y(X_hout)
 
-    X_calib = data_model.sample_X(n_calib)              # Generate independent calibration data
+    # Generate independent calibration data
+    X_calib = data_model.sample_X(n_calib)
     Y_calib = data_model.sample_Y(X_calib)
 
-    X_test = data_model.sample_X(n_test, test=True)     # Generate independent test data
+    # Generate independent test data
+    X_test = data_model.sample_X(n_test, test=True)
     Y_test = data_model.sample_Y(X_test)
 
     X_augmented = np.concatenate((X_train, X_tr_score), 0)
@@ -205,35 +219,40 @@ def setup_data_and_model(device):
     Z_tr_score = np.ones(len(Y_tr_score))
     Z_augmented = np.concatenate((Z_train, Z_tr_score), 0)
 
-    # Initialize loader for training data 
+    # Initialize loader for training data
     X_train = torch.from_numpy(X_augmented).float().to(device)
     Y_train = torch.from_numpy(Y_augmented).long().to(device)
     Z_train = torch.from_numpy(Z_augmented).long().to(device)
     train_dataset = ClassifierDataset(X_train, Y_train, Z_train)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
     # Initialize loader for hold-out data (if available)
     X_hout = torch.from_numpy(X_hout).float().to(device)
     Y_hout = torch.from_numpy(Y_hout).long().to(device)
     Z_hout = torch.ones(Y_hout.shape).long().to(device)
     val_dataset = ClassifierDataset(X_hout, Y_hout, Z_hout)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-    # Initialize loader for calibration data 
+    # Initialize loader for calibration data
     X_calib = torch.from_numpy(X_calib).float().to(device)
     Y_calib = torch.from_numpy(Y_calib).long().to(device)
     cal_dataset = CommonDataset(X_calib, Y_calib)
-    cal_loader = DataLoader(cal_dataset, batch_size=100, shuffle=True, drop_last=True)
+    cal_loader = DataLoader(cal_dataset, batch_size=100,
+                            shuffle=True, drop_last=True)
 
-    # Initialize loader for test data 
+    # Initialize loader for test data
     X_test = torch.from_numpy(X_test).float().to(device)
     Y_test = torch.from_numpy(Y_test).long().to(device)
     test_dataset = CommonDataset(X_test, Y_test)
-    test_loader = DataLoader(test_dataset, batch_size=100, shuffle=True, drop_last=True)
+    test_loader = DataLoader(test_dataset, batch_size=100,
+                             shuffle=True, drop_last=True)
 
     oracle = Oracle(data_model)
 
-    model = ClassNNet(num_features=p, num_classes=K, use_dropout=False).to(device)
+    model = ClassNNet(num_features=p, num_classes=K,
+                      use_dropout=False).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     return train_loader, val_loader, cal_loader, X_test, Y_test, oracle, model, optimizer
@@ -250,56 +269,63 @@ if __name__ == '__main__':
 
     checkpoint_path = os.path.join(get_others_dir(), "conflearn")
 
-    train_loader, val_loader, cal_loader, X_test, Y_test, oracle, model, optimizer = setup_data_and_model(device)
+    train_loader, val_loader, cal_loader, X_test, Y_test, oracle, model, optimizer = setup_data_and_model(
+        device)
 
     conflearn_trainer = ConfLearnTrainer(model, optimizer, device=device)
-    conflearn_trainer.train(train_loader, val_loader, checkpoint_path=checkpoint_path, num_epochs=4000)
-    
+    conflearn_trainer.train(train_loader, val_loader,
+                            checkpoint_path=checkpoint_path, num_epochs=4000)
+
     # For early stopping loss
     conflearn_trainer_loss = ConfLearnTrainer(model, optimizer, device=device)
     if os.path.exists(checkpoint_path + "_loss"):
         conflearn_trainer_loss.load_checkpoint(checkpoint_path + "_loss")
     else:
-       conflearn_trainer_loss.load_checkpoint(checkpoint_path + "_final")
+        conflearn_trainer_loss.load_checkpoint(checkpoint_path + "_final")
 
     # For early stopping acc
     conflearn_trainer_acc = ConfLearnTrainer(model, optimizer, device=device)
     if os.path.exists(checkpoint_path + "_acc"):
         conflearn_trainer_loss.load_checkpoint(checkpoint_path + "_acc")
     else:
-       conflearn_trainer_loss.load_checkpoint(checkpoint_path + "_final")
+        conflearn_trainer_loss.load_checkpoint(checkpoint_path + "_final")
 
-    black_boxes = [conflearn_trainer, conflearn_trainer_loss, conflearn_trainer_acc]
+    black_boxes = [conflearn_trainer,
+                   conflearn_trainer_loss, conflearn_trainer_acc]
 
 
     sc_methods = []
     for i in range(len(black_boxes)):
-       sc_method = SplitPredictor(APS(), black_boxes[i].model)
-       sc_method.calibrate(cal_loader, alpha)
-       sc_methods.append(sc_method)
+        sc_method = SplitPredictor(APS(), black_boxes[i].model)
+        sc_method.calibrate(cal_loader, alpha)
+        sc_methods.append(sc_method)
 
     results = pd.DataFrame()
 
     sc_method_oracle = SplitPredictor(APS(score_type="identity"))
     sc_method_oracle._device = device
     pred_prob_oracle = oracle.predict_proba(X_test.cpu().numpy())
-    sets_oracle = sc_method_oracle.predict_with_logits(torch.tensor(pred_prob_oracle), 1 - alpha)
+    sets_oracle = sc_method_oracle.predict_with_logits(
+        torch.tensor(pred_prob_oracle), 1 - alpha)
     row_indices, col_indices = sets_oracle.nonzero(as_tuple=True)
-    sets_oracle = [col_indices[row_indices == i].tolist() for i in range(sets_oracle.size(0))]
+    sets_oracle = [col_indices[row_indices == i].tolist()
+                   for i in range(sets_oracle.size(0))]
 
     easy_idx, hard_idx = difficulty_oracle(sets_oracle)
-    
+
 
     for k in range(len(black_boxes)):
         sets = sc_methods[k].predict(X_test)
         row_indices, col_indices = sets.nonzero(as_tuple=True)
-        sets = [col_indices[row_indices == i].tolist() for i in range(sets.size(0))]
+        sets = [col_indices[row_indices == i].tolist()
+                for i in range(sets.size(0))]
 
-        res = evaluate_predictions(sets, X_test, Y_test, hard_idx, conditional=True)
+        res = evaluate_predictions(
+            sets, X_test, Y_test, hard_idx, conditional=True)
         # res['Error'] = eval_predictions(X_test, Y_test, black_boxes[k], data="test")
 
         results = pd.concat([results, res])
-        
+
     results = results.reset_index()
 
     print(results)
--- torchcp/classification/trainer/conflearn_trainer.py
+++ torchcp/classification/trainer/conflearn_trainer.py
@@ -14,6 +14,7 @@ from torchsort import soft_rank, soft_sort
 REG_STRENGTH = 0.1
 B = 50
 
+
 class UniformMatchingLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -22,17 +23,19 @@ class UniformMatchingLoss(torch.nn.Module):
         batch_size = len(x)
         if batch_size == 0:
             return 0
-        x_sorted = soft_sort(x.unsqueeze(dim=0), regularization_strength=REG_STRENGTH)
-        i_seq = torch.arange(1.0, 1.0 + batch_size, device=x.device)/(batch_size)
+        x_sorted = soft_sort(x.unsqueeze(
+            dim=0), regularization_strength=REG_STRENGTH)
+        i_seq = torch.arange(1.0, 1.0 + batch_size,
+                             device=x.device) / (batch_size)
         out = torch.max(torch.abs(i_seq - x_sorted))
         return out
-    
+
 
 def soft_indicator(x, a, b=B):
     def sigmoid(x):
         return 1.0 / (1.0 + np.exp(-x))
     out = torch.sigmoid(b * (x - a + 0.5)) - (torch.sigmoid(b * (x - a - 0.5)))
-    out = out / (sigmoid(b * (0.5)) - (sigmoid(b * (-0.5))) )
+    out = out / (sigmoid(b * (0.5)) - (sigmoid(b * (-0.5))))
     return out
 
 
@@ -46,26 +49,27 @@ def soft_indexing(z, rank):
 
 
 def accuracy_point(y_pred, y_test):
-    y_pred_softmax = torch.log_softmax(y_pred, dim = 1)
-    _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)
+    y_pred_softmax = torch.log_softmax(y_pred, dim=1)
+    _, y_pred_tags = torch.max(y_pred_softmax, dim=1)
 
     correct_pred = (y_pred_tags == y_test).float()
     acc = correct_pred.sum() / len(correct_pred)
 
-    return acc*100
-  
+    return acc * 100
+
 
 class ConfLearnTrainer:
 
-    def __init__(self, 
+    def __init__(self,
                  model: torch.nn.Module,
                  optimizer: torch.optim.Optimizer,
                  criterion_pred_loss_fn=torch.nn.CrossEntropyLoss(),
-                 mu: float=0.2,
-                 alpha: float=0.1,
-                 device: torch.device=None):
+                 mu: float = 0.2,
+                 alpha: float = 0.1,
+                 device: torch.device = None):
         if device is None:
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            self.device = torch.device(
+                'cuda' if torch.cuda.is_available() else 'cpu')
         else:
             self.device = device
 
@@ -81,29 +85,36 @@ class ConfLearnTrainer:
 
     def compute_scores_diff(self, proba_values, Y_values, alpha=0.1):
         n, K = proba_values.shape
-        proba_values = proba_values + 1e-6 * torch.rand(proba_values.shape, dtype=float, device=self.device)
-        proba_values = proba_values / torch.sum(proba_values,1)[:,None]
-        ranks_array_t = soft_rank(-proba_values, regularization_strength=REG_STRENGTH)-1
-        prob_sort_t = -soft_sort(-proba_values, regularization_strength=REG_STRENGTH)
+        proba_values = proba_values + 1e-6 * \
+            torch.rand(proba_values.shape, dtype=float, device=self.device)
+        proba_values = proba_values / torch.sum(proba_values, 1)[:, None]
+        ranks_array_t = soft_rank(-proba_values,
+                                  regularization_strength=REG_STRENGTH) - 1
+        prob_sort_t = -soft_sort(-proba_values,
+                                 regularization_strength=REG_STRENGTH)
         Z_t = prob_sort_t.cumsum(dim=1)
 
-        ranks_t = torch.gather(ranks_array_t, 1, Y_values.reshape(n,1)).flatten()
+        ranks_t = torch.gather(
+            ranks_array_t, 1, Y_values.reshape(n, 1)).flatten()
         prob_cum_t = soft_indexing(Z_t, ranks_t)
         prob_final_t = soft_indexing(prob_sort_t, ranks_t)
-        scores_t = 1.0 - prob_cum_t + prob_final_t * torch.rand(n,dtype=float,device=self.device)
+        scores_t = 1.0 - prob_cum_t + prob_final_t * \
+            torch.rand(n, dtype=float, device=self.device)
 
         return scores_t
 
     def compute_loss_scores(self, y_train_pred, y_train_batch, alpha=0.1):
         train_proba = self.layer_prob(y_train_pred)
-        train_scores = self.compute_scores_diff(train_proba, y_train_batch, alpha=alpha)
+        train_scores = self.compute_scores_diff(
+            train_proba, y_train_batch, alpha=alpha)
         train_loss_scores = self.criterion_scores(train_scores)
         return train_loss_scores
 
     def calculate_loss(self, output, target, Z_batch, training=True):
         if training:
             idx_ce = torch.where(Z_batch == 0)[0]
-            loss_ce = self.criterion_pred_loss_fn(output[idx_ce], target[idx_ce])
+            loss_ce = self.criterion_pred_loss_fn(
+                output[idx_ce], target[idx_ce])
         else:
             loss_ce = self.criterion_pred_loss_fn(output, target)
 
@@ -113,7 +124,8 @@ class ConfLearnTrainer:
         for z in Z_groups:
             if z > 0:
                 idx_z = torch.where(Z_batch == z)[0]
-                loss_scores_z = self.compute_loss_scores(output[idx_z], target[idx_z], alpha=self.alpha)
+                loss_scores_z = self.compute_loss_scores(
+                    output[idx_z], target[idx_z], alpha=self.alpha)
                 loss_scores += loss_scores_z
         loss_scores /= n_groups
 
@@ -149,55 +161,56 @@ class ConfLearnTrainer:
 
             loss_val += loss.item()
             acc_val += acc.item()
-        
+
         loss_val /= len(val_loader)
         acc_val /= len(val_loader)
 
         return loss_val, acc_val
 
-    def train(self, 
-              train_loader, 
-              val_loader=None, 
-              early_stopping=True, 
-              save_model=True, 
-              checkpoint_path: str=None, 
+    def train(self,
+              train_loader,
+              val_loader=None,
+              early_stopping=True,
+              save_model=True,
+              checkpoint_path: str = None,
               num_epochs=10):
         if save_model:
             if checkpoint_path is None:
-                raise("Output checkpoint name file is needed.")
-            
+                raise ("Output checkpoint name file is needed.")
+
         best_loss = None
         best_acc = None
 
-        lr_milestones = [int(num_epochs*0.5)]
-        scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=lr_milestones, gamma=0.1)
+        lr_milestones = [int(num_epochs * 0.5)]
+        scheduler = optim.lr_scheduler.MultiStepLR(
+            self.optimizer, milestones=lr_milestones, gamma=0.1)
 
         for epoch in tqdm(range(num_epochs)):
 
             self.train_epoch(train_loader)
-            
+
             scheduler.step()
             self.model.eval()
 
             if val_loader is not None:
                 epoch_loss_val, epoch_acc_val = self.validate(val_loader)
-            
+
             if early_stopping:
                 # Early stopping by loss
                 save_checkpoint = True if best_loss is not None and best_loss > epoch_loss_val else False
                 best_loss = epoch_loss_val if best_loss is None or best_loss > epoch_loss_val else best_loss
                 if save_checkpoint:
-                    self.save_checkpoint(epoch, checkpoint_path+"_loss")
-                
+                    self.save_checkpoint(epoch, checkpoint_path + "_loss")
+
                 # Early stopping by accuracy
                 save_checkpoint = True if best_acc is not None and best_acc < epoch_acc_val else False
                 best_acc = epoch_acc_val if best_acc is None or best_acc < epoch_acc_val else best_acc
                 if save_checkpoint:
-                    self.save_checkpoint(epoch, checkpoint_path+"_acc")
+                    self.save_checkpoint(epoch, checkpoint_path + "_acc")
 
         if save_model:
-            self.save_checkpoint(epoch, checkpoint_path+"_final")
-        
+            self.save_checkpoint(epoch, checkpoint_path + "_final")
+
 
     def save_checkpoint(self, epoch: int, save_path: str):
         checkpoint = {
@@ -212,7 +225,7 @@ class ConfLearnTrainer:
         self.model.load_state_dict(checkpoint['model_state_dict'])
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         return checkpoint
-    
+
     def predict(self, test_loader):
         y_pred_list = []
         with torch.no_grad():
@@ -220,8 +233,8 @@ class ConfLearnTrainer:
             for X_batch, _ in test_loader:
                 X_batch = X_batch.to(self.device)
                 y_test_pred = self.model(X_batch)
-                y_pred_softmax = torch.log_softmax(y_test_pred, dim = 1)
-                _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)
+                y_pred_softmax = torch.log_softmax(y_test_pred, dim=1)
+                _, y_pred_tags = torch.max(y_pred_softmax, dim=1)
                 y_pred_list.append(y_pred_tags.cpu().numpy())
         y_pred = np.concatenate(y_pred_list)
         return y_pred
