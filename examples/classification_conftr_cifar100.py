164749715530919873e0adff2f447f453883f9a9
janqsong <janqsong@gmail.com>
2025-06-17 21:13:02 +0800

    add example, test, annotation of conftr

---
+++ examples/classification_conftr_cifar100.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2023-present, SUSTech-ML.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import torch
+import torchvision
+import torchvision.transforms as trn
+from transformers import set_seed
+
+from examples.utils import get_dataset_dir
+from torchcp.classification.predictor import SplitPredictor
+from torchcp.classification.score import APS
+from torchcp.classification.trainer import ConfTrTrainer
+
+set_seed(seed=2025)
+
+#######################################
+# Preparing a calibration data and a test data
+#######################################
+transform = trn.Compose([
+    trn.ToTensor(),
+    trn.Normalize(mean=[0.5071, 0.4867, 0.4408],
+                  std=[0.2675, 0.2565, 0.2761])
+])
+dataset = torchvision.datasets.CIFAR100(
+    root=get_dataset_dir(),
+    train=False,
+    download=True,
+    transform=transform
+)
+cal_dataset, conformal_cal_dataset, test_dataset = torch.utils.data.random_split(dataset, [3000, 2000, 5000])
+cal_dataloader = torch.utils.data.DataLoader(cal_dataset, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
+conformal_cal_dataloader = torch.utils.data.DataLoader(conformal_cal_dataset, batch_size=64, shuffle=False,
+                                                       num_workers=4, pin_memory=True)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4,
+                                              pin_memory=True)
+
+#######################################
+# Preparing a pytorch model
+#######################################
+model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar100_resnet20", pretrained=True)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+model.eval()
+
+#######################################
+# Temperature Scaling
+#######################################
+
+trainer = ConfTrTrainer(
+    alpha=0.1,
+    model=model,
+    device=device,
+    verbose=True
+)
+
+calibrated_model = trainer.train(cal_dataloader, num_epochs=10)
+
+########################################
+# Conformal prediction
+########################################
+
+predictor = SplitPredictor(score_function=APS(), model=calibrated_model)
+predictor.calibrate(conformal_cal_dataloader, alpha=0.1)
+result_dict = predictor.evaluate(test_dataloader)
+print(f"Coverage Rate: {result_dict['coverage_rate']:.4f}")
+print(f"Average Set Size: {result_dict['average_size']:.4f}")
--- torchcp/classification/trainer/conftr_trainer.py
+++ torchcp/classification/trainer/conftr_trainer.py
@@ -16,7 +16,37 @@ from torchcp.classification.trainer.model_zoo import TemperatureScalingModel
 
 
 class ConfTrTrainer(Trainer):
-    """Conformal Training Trainer."""
+    """Conformal Training Trainer.
+
+    Args:
+        alpha (float): Target miscoverage rate (significance level) for conformal prediction.
+        model (torch.nn.Module): Base neural network model to be calibrated.
+        device (torch.device, optional): Device to run the model on. If None, will automatically use GPU ('cuda') if available, otherwise CPU ('cpu')
+            Default: None
+        verbose (bool): Whether to display training progress. Default: True.
+
+        Examples:
+            >>> # Initialize a CNN model
+            >>> cnn = torchvision.models.resnet18(pretrained=True)
+            >>> 
+            >>> # Create ConfTr trainer
+            >>> trainer = ConfTrTrainer(
+            ...     alpha=0.1    
+            ...     model=cnn)
+            >>> 
+            >>> # Train calibration
+            >>> trainer.train(
+            ...     train_loader=train_loader,
+            ...     val_loader=val_loader,
+            ...     num_epochs=10
+            ... )
+            >>> 
+            >>> # Save calibrated model
+            >>> trainer.save_model('calibrated_model.pth')
+    
+        Reference:
+            Stutz et al. "Learning Optimal Conformal Classifiers" (2022), https://arxiv.org/abs/2110.09192
+    """
 
     def __init__(
             self,
